c40735a8f17760636e993f2fad145edbcc51e9f9
Dhyanesh-Panchal <dhynincoding2004@gmail.com>
2025-04-14 11:02:14 +0530

    Little bit modularization in reader

--- reportdb/src/datastore/reader/query.go
+++ reportdb/src/datastore/reader/query.go
@@ -1,6 +1,12 @@
 package reader
 
-import . "datastore/containers"
+import (
+	. "datastore/containers"
+	. "datastore/storage"
+	"errors"
+	"log"
+	"sync"
+)
 
 type Query struct {
 	QueryId     uint64   `json:"query_id"`
@@ -15,4 +21,70 @@ type Result struct {
 	QueryId uint64 `json:"query_id"`
 
 	Data map[uint32][]DataPoint `json:"data"`
+}
+
+func InitQueryHandler(queryReceiveChannel <-chan Query, queryResultChannel chan<- Result, storagePool *StoragePool, shutdownWaitGroup *sync.WaitGroup) {
+
+	defer shutdownWaitGroup.Done()
+
+	for query := range queryReceiveChannel {
+
+		result, err := queryHistogram(query.From, query.To, query.CounterId, query.ObjectIds, storagePool)
+
+		if err != nil {
+
+			log.Printf("Error querying datastore: %s", err)
+
+		}
+
+		queryResultChannel <- Result{
+
+			QueryId: query.QueryId,
+
+			Data: result,
+		}
+
+	}
+
+	close(queryResultChannel)
+
+}
+
+func queryHistogram(from uint32, to uint32, counterId uint16, objects []uint32, storagePool *StoragePool) (map[uint32][]DataPoint, error) {
+
+	finalData := map[uint32][]DataPoint{}
+
+	for date := from - (from % 86400); date <= to; date += 86400 {
+
+		dateObject := UnixToDate(date)
+
+		storageKey := StoragePoolKey{
+
+			Date: dateObject,
+
+			CounterId: counterId,
+		}
+
+		storageEngine, err := storagePool.GetStorage(storageKey, false)
+
+		if err != nil {
+
+			if errors.Is(err, ErrStorageDoesNotExist) {
+
+				continue
+
+			}
+
+			return nil, err
+
+		}
+
+		readSingleDay(dateObject, storageEngine, counterId, objects, finalData, from, to)
+
+		storagePool.CloseStorage(storageKey)
+
+	}
+
+	return finalData, nil
+
 }
--- reportdb/src/datastore/reader/reader.go
+++ reportdb/src/datastore/reader/reader.go
@@ -4,76 +4,8 @@ import (
 	. "datastore/containers"
 	. "datastore/storage"
 	. "datastore/utils"
-	"errors"
 	"log"
-	"sync"
 )
-
-func InitQueryHandler(queryReceiveChannel <-chan Query, queryResultChannel chan<- Result, storagePool *StoragePool, shutdownWaitGroup *sync.WaitGroup) {
-
-	defer shutdownWaitGroup.Done()
-
-	for query := range queryReceiveChannel {
-
-		result, err := queryHistogram(query.From, query.To, query.CounterId, query.ObjectIds, storagePool)
-
-		if err != nil {
-
-			log.Printf("Error querying datastore: %s", err)
-
-		}
-
-		queryResultChannel <- Result{
-
-			QueryId: query.QueryId,
-
-			Data: result,
-		}
-
-	}
-
-	close(queryResultChannel)
-
-}
-
-func queryHistogram(from uint32, to uint32, counterId uint16, objects []uint32, storagePool *StoragePool) (map[uint32][]DataPoint, error) {
-
-	finalData := map[uint32][]DataPoint{}
-
-	for date := from - (from % 86400); date <= to; date += 86400 {
-
-		dateObject := UnixToDate(date)
-
-		storageKey := StoragePoolKey{
-
-			Date: dateObject,
-
-			CounterId: counterId,
-		}
-
-		storageEngine, err := storagePool.GetStorage(storageKey, false)
-
-		if err != nil {
-
-			if errors.Is(err, ErrStorageDoesNotExist) {
-
-				continue
-
-			}
-
-			return nil, err
-
-		}
-
-		readSingleDay(dateObject, storageEngine, counterId, objects, finalData, from, to)
-
-		storagePool.CloseStorage(storageKey)
-
-	}
-
-	return finalData, nil
-
-}
 
 // readSingleDay Note: readFullDate function changes the state of the finalData; hence if run in parallel, proper synchronization is needed.
 func readSingleDay(date Date, storageEngine *Storage, counterId uint16, objects []uint32, finalData map[uint32][]DataPoint, from uint32, to uint32) {
