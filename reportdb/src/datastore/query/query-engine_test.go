b0fc7c550fa715250f2f249beaaeceb40ebbd6f7
Dhyanesh-Panchal <dhynincoding2004@gmail.com>
2025-05-07 14:53:54 +0530

    Changes in the query-engine test cases

--- reportdb/src/datastore/query/query-engine_test.go
+++ reportdb/src/datastore/query/query-engine_test.go
@@ -12,6 +12,8 @@ func TestQueryEngine(t *testing.T) {
 
 	_ = utils.LoadConfig()
 
+	_ = utils.InitLogger()
+
 	queryReceiveChannel := make(chan Query, 10)
 
 	queryResultChannel := make(chan Result, 10)
@@ -27,9 +29,9 @@ func TestQueryEngine(t *testing.T) {
 	query := Query{
 		QueryId:               10,
 		CounterId:             2,
-		From:                  1746037914,
-		To:                    1746041514,
-		ObjectIds:             []uint32{2130706433},
+		From:                  1746505800,
+		To:                    1746541800,
+		ObjectIds:             []uint32{2886731847},
 		VerticalAggregation:   "none",
 		HorizontalAggregation: "none",
 		Interval:              0,
@@ -37,6 +39,45 @@ func TestQueryEngine(t *testing.T) {
 
 	queryReceiveChannel <- query
 
-	fmt.Println(<-queryResultChannel)
+	result := <-queryResultChannel
+
+	fmt.Println(result.Data[2886731847])
+
+}
+
+func TestQueryEngine2(t *testing.T) {
+
+	_ = utils.LoadConfig()
+
+	_ = utils.InitLogger()
+
+	queryReceiveChannel := make(chan Query, 10)
+
+	queryResultChannel := make(chan Result, 10)
+
+	storagePool := containers.NewOpenStoragePool()
+
+	var shutdownWaitGroup sync.WaitGroup
+
+	shutdownWaitGroup.Add(1)
+
+	go InitQueryEngine(queryReceiveChannel, queryResultChannel, storagePool, &shutdownWaitGroup)
+
+	query := Query{
+		QueryId:               1,
+		CounterId:             2,
+		From:                  1746515997,
+		To:                    1746541800,
+		ObjectIds:             []uint32{2886731920},
+		VerticalAggregation:   "none",
+		HorizontalAggregation: "none",
+		Interval:              0,
+	}
+
+	queryReceiveChannel <- query
+
+	result := <-queryResultChannel
+
+	fmt.Println(len(result.Data[2886731920]))
 
 }
